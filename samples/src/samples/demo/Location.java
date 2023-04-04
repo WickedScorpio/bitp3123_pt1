064b712ae52a348b1908132ca1aca7abd8e0a9f1
Mohamad Zulkarnain Bin Mohd Anuar <zulhelious@gmail.com>
2023-04-04 16:31:33 +0800

    Exercise

---
+++ samples/src/samples/demo/Location.java
@@ -0,0 +1,23 @@
+package samples.demo;
+
+public class Location {
+
+	private String tempat;
+	private String negara;
+	
+	public String getTempat() {
+		return tempat;
+	}
+	public void setTempat(String tempat) {
+		this.tempat = tempat;
+	}
+	public String getNegara() {
+		return negara;
+	}
+	public void setNegara(String negara) {
+		this.negara = negara;
+	}
+	
+}
+
+ 
