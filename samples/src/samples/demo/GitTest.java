c46c18250daccd1eb50d6ec7c15daee31fa656c2
Mohamad Zulkarnain Bin Mohd Anuar <zulhelious@gmail.com>
2023-04-04 15:57:52 +0800

    Initial Commit

---
+++ samples/src/samples/demo/GitTest.java
@@ -0,0 +1,12 @@
+package samples.demo;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Git is fun");
+
+	}
+
+}
